14e6ef05fcd9bd0b9f1ee96ed893fdad9ce9d3c9
Tom Nielsen <tanielsen@gmail.com>
2026-02-20 15:45:31 +0000

    state params test

--- tests/sqlview.test.js
+++ tests/sqlview.test.js
@@ -38,4 +38,24 @@ describe("sql view", () => {
       '<script>const boooks = [{"id":1,"author":"Herman Melville","pages":967},{"id":2,"author":"Leo Tolstoy","pages":728}]<script>',
     );
   });
+  it("run with state params", async () => {
+    const view = new View({
+      name: "BookSQLView",
+      description: "",
+      viewtemplate: "SQLView",
+      configuration: {
+        sql: "select id, author, pages from books where id = $1",
+        html_code: `<script>const boooks = {{ JSON.stringify(rows) }}<script>`,
+        output_type: "HTML",
+        state_parameters: "id",
+      },
+      min_role: 1,
+      table: null,
+    });
+    const result = await view.run({id:2}, mockReqRes);
+
+    expect(result).toBe(
+      '<script>const boooks = [{"id":2,"author":"Leo Tolstoy","pages":728}]<script>',
+    );
+  });
 });
