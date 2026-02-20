4323a26980d533b9942c9dfd8910536e5ede4177
Tom Nielsen <tanielsen@gmail.com>
2026-02-20 15:38:07 +0000

    sql view test

---
+++ tests/sqlview.test.js
@@ -0,0 +1,41 @@
+const { getState } = require("@saltcorn/data/db/state");
+const View = require("@saltcorn/data/models/view");
+const Table = require("@saltcorn/data/models/table");
+const Plugin = require("@saltcorn/data/models/plugin");
+
+const { mockReqRes } = require("@saltcorn/data/tests/mocks");
+const { afterAll, beforeAll, describe, it, expect } = require("@jest/globals");
+const db = require("@saltcorn/data/db");
+
+afterAll(require("@saltcorn/data/db").close);
+beforeAll(async () => {
+  await require("@saltcorn/data/db/reset_schema")();
+  await require("@saltcorn/data/db/fixtures")();
+
+  getState().registerPlugin("base", require("@saltcorn/data/base-plugin"));
+  getState().registerPlugin("@saltcorn/sql", require(".."));
+  //db.set_sql_logging(true);
+});
+
+describe("sql view", () => {
+  it("runs", async () => {
+    const view = new View({
+      name: "BookSQLView",
+      description: "",
+      viewtemplate: "SQLView",
+      configuration: {
+        sql: "select id, author, pages from books order by id;",
+        html_code: `<script>const boooks = {{ JSON.stringify(rows) }}<script>`,
+        output_type: "HTML",
+        state_parameters: "",
+      },
+      min_role: 1,
+      table: null,
+    });
+    const result = await view.run({}, mockReqRes);
+
+    expect(result).toBe(
+      '<script>const boooks = [{"id":1,"author":"Herman Melville","pages":967},{"id":2,"author":"Leo Tolstoy","pages":728}]<script>',
+    );
+  });
+});
